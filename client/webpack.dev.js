b7493dbbb1c4b6bd4a0ad6706820aed1633aa6d4
WesamKhalil <wkhalil@tutamail.com>
2021-03-20 15:27:50 +0000

    Fixed mistake, webpack.dev.js connected to the .env file.

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,17 +1,18 @@
+const path = require("path")
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const path = require("path")
+require('../node_modules/dotenv').config({ path: __dirname + '../../.env' })
 
 module.exports = merge(common, {
     mode: "development",
     devtool: 'inline-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, "dist"),
-        port: process.env.CLIENT_PORT || 8080,
+        port: (process.env.CLIENT_PORT || 8080),
         open: true,
         hot: true,
         proxy: {
-            "/api": "http://localhost:" + process.env.SERVER_PORT || 3000
+            "/api": "http://localhost:" + (process.env.SERVER_PORT || 3000)
         },
         historyApiFallback: true
     },
